1d69bdd1b14d43d03bf82c2714eb3039c1c7b572
denis@dschoener.de <denis@dschoener.de>
2020-02-15 18:11:50 +0100

    Add flash script and some todos

---
+++ flash-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+IMAGE=./build/tmp/deploy/images/raspberrypi3/core-image-base-moco-raspberrypi3.rpi-sdimg
+SD_MEM=/dev/mmcblk0
+if ! [[ -f "$IMAGE" ]]; then
+  echo "error: image not found: $IMAGE"
+  exit
+fi
+if ! [[ -b "$SD_MEM" ]]; then
+  echo "error: memory card not found"
+  exit
+fi
+dd if="$IMAGE" of="$SD_MEM" bs=10M
